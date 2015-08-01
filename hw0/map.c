3d2bcea7526da79e4f78e0f605c1928f0518e6bd
abdulkadirdere <752817@students.wits.ac.za>
2015-08-01 18:20:57 +0200

    hw0 try

--- hw0/map.c
+++ hw0/map.c
@@ -5,23 +5,23 @@ int foo;            /* a staticly allocated variable */
 
 int recur(int i) {      /* a recursive function */
     int j = i;            /* a stack allocated variable within a recursive function */
-    printf("recur call %d: stack@ %lx\n", i, j); /* fix this so it prints the address of j */
+    printf("recur call %d: stack@ %lx\n", i, (long) &j); /* %l is for long type */
     if (i > 0) {
         return recur(i-1);
     }
     return 0;
 }
 
-int stuff = 7;          /* a statically allocarted, pre-initialized variable */
+int stuff = 7;          /* a statically allocated, pre-initialized variable */
 
 int main (int argc, char *argv[]) {
     int i;            /* a stack allocated variable */
-    char *buf1 = malloc(100); /* dynamically allocate some stuff */
-    char *buf2 = malloc(100); /* and some more stuff */
-    printf("_main  @ %lx\n", main); /* fix to print address of main */
-    printf("recur @ %lx\n", recur); /* fix to print address of recur */
-    printf("_main stack: %lx\n", i); /* fix to get address of the stack variable */
-    printf("static data: %lx\n", stuff); /* fix to get address of a static variable */
+    char *buf1 =(char*) malloc(100); /* dynamically allocate stuff but cast it */
+    char *buf2 =(char*) malloc(100); /* and some more stuff with casting */
+    printf("_main  @ %lx\n",(long) &main); /*main needs & = pointing to an address*/
+    printf("recur @ %lx\n",(long) recur); /*  %l is a type long */
+    printf("_main stack: %lx\n",(long) i); /* & = address of stack variable */
+    printf("static data: %lx\n",(long) &stuff); /* &=address */
     printf("Heap: malloc 1: %lx\n", (long unsigned int) buf1);
     printf("Heap: malloc 2: %lx\n", (long unsigned int) buf2);
     recur(3);
--- hw0/wc.c
+++ hw0/wc.c
@@ -5,12 +5,32 @@
 
 void wc ( FILE *infile, char *inname);
 
-int
-main (int argc, char *argv[]) {
-    
+int main (int argc, char *argv[]) {
+	FILE *infile;
+	char ch;	
 }
 
-void
-wc( FILE *infile, char *inname) {
-    
+void wc(FILE *infile, char *inname) {
+	FILE *infile;
+	char ch;
+	int number,nl,nt,nb,nc=0; //intialise number of lines, tabs, blank spaces and characters to 0
+
+	infile=fopen(argv[1],"r");
+	while(1)
+	{
+		ch=fgetc(infile);
+		if(ch==EOF)
+			break;
+			nc++; //to count number of characters
+		if(ch==' ')
+			nb++; //to count number of blank spaces
+		if(ch=='\n')
+			nl++; //to count number of lines
+		if(ch=='\t')
+			nt++; //to count number of tabs
+	}
+	number = nl+nt+nb+nc;
+	fclose(infile);
+	printf("Total number of character in file = %d \n",number);
+	return 0;
 }
